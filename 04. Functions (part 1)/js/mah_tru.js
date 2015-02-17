f1eb3bd2b2e6c707de0403f8dba4937a0df07865
U-IFAO-BG\velikova <velikova@velikova.sofia.ifao.net>
2015-02-17 11:46:16 +0200

    removes unneeded js

--- 04. Functions (part 1)/js/mah_tru.js
+++
@@ -1,9 +0,0 @@
-function typeOf (variable) {
-  var type = ({}).toString.call(variable).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
-  
-  if (isNaN(variable)) {
-    return 'NaN';
-  }
-
-  return type;
-}
