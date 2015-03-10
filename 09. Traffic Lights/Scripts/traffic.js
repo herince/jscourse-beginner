99a0aa59b3ae83c46eb445d72e6b59974e1d7058
Ralitsa Velikova <rherince@gmail.com>
2015-03-10 12:45:04 +0200

    renaming colors to lights

--- 09. Traffic Lights/Scripts/traffic.js
+++ 09. Traffic Lights/Scripts/traffic.js
@@ -11,16 +11,16 @@ var TrafficLight = function (type) {
   }
 
   this.type = type;
-  this.colors = {};
+  this.lights = {};
 
   if (type == 'cars') {
-    this.colors = {
+    this.lights = {
       red: new Light('red'),
       yellow: new Light('yellow'),
       green: new Light('green')
     };
   } else {
-    this.colors = {
+    this.lights = {
       red: new Light('red'),
       green: new Light('green')
     };
@@ -31,20 +31,20 @@ TrafficLight.prototype = {
   /* stops traffic by turning on only the red light */
   stop: function () {
     if (this.type == 'cars') {
-      this.colors.yellow.turnOff();
+      this.lights.yellow.turnOff();
     }
 
-    this.colors.green.turnOff();
-    this.colors.red.turnOn();
+    this.lights.green.turnOff();
+    this.lights.red.turnOn();
   },
 
   /* allows cars/humans to pass by turning only the green light on */
   pass: function () {
     if (this.type == 'cars') {
-      this.colors.yellow.turnOff();
+      this.lights.yellow.turnOff();
     }
 
-    this.colors.red.turnOff();
-    this.colors.green.turnOn();
+    this.lights.red.turnOff();
+    this.lights.green.turnOn();
   }
 }
