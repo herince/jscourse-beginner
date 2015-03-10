648b5042e74085d86beb607a7f50af404de4a1fd
Ralitsa Velikova <rherince@gmail.com>
2015-03-10 12:38:26 +0200

    new homework

---
+++ 09. Traffic Lights/Scripts/light.js
@@ -0,0 +1,39 @@
+/** 
+ * @class  Light
+ * 
+ * @requires  Class
+ */
+
+var Light = function (color) {
+  this.color = color;
+  this.is_on = false;
+}
+
+Light.prototype = {
+   
+  constructor: Light,
+   
+  states: [ 'off', 'on' ],
+   
+  turnOn: function () {
+    this.switch(true);
+  },
+   
+  turnOff: function () {
+    this.switch(false);
+  },
+   
+  switch: function (is_on) {
+    if (this.is_on == ! is_on) {
+      this.is_on = is_on;
+      console.info(this.color + ' has been turned ' + this.states[ new Number(is_on) ]);
+      return this.is_on;
+    }
+    console.warn(this.color + ' is already ' + this.states[ new Number(this.is_on) ]);
+    return this.is_on;
+  },
+   
+  isOn: function () {
+    return this.is_on;
+  }
+}
---
+++ 09. Traffic Lights/Scripts/traffic.js
@@ -0,0 +1,54 @@
+/** 
+ * @class  TrafficLight
+ * 
+ * @requires  Class
+ */
+
+var TrafficLight = function (type) {
+  if (type === undefined || (type !== 'cars' && type !== 'humans')) {
+    throw 'Invalid traffic lights type!';
+    return;
+  }
+
+  this.type = type;
+  this.colors = {};
+
+  if (type == 'cars') {
+    this.colors = {
+      red: new Light('red'),
+      yellow: new Light('yellow'),
+      green: new Light('green')
+    };
+  } else {
+    this.colors = {
+      red: new Light('red'),
+      green: new Light('green')
+    };
+  }
+}
+
+TrafficLight.prototype = {
+  /* stops traffic by turning on only the red light */
+  stop: function () {
+    if (this.type == 'cars') {
+      this.colors.green.turnOff();
+      this.colors.yellow.turnOff();
+    } else {
+      this.colors.green.turnOff();
+    }
+
+    this.colors.red.turnOn();
+  },
+
+  /* allows cars/humans to pass by turning only the green light on */
+  pass: function () {
+    if (this.type == 'cars') {
+      this.colors.red.turnOff();
+      this.colors.yellow.turnOff();
+    } else if (this.type == 'humans') {
+      this.colors.red.turnOff();
+    }
+
+    this.colors.green.turnOn();
+  }
+}
